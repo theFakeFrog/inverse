93787c02e25c11352dfb1cf35314ce2dca625115
TheFakeFrog <116329407+theFakeFrog@users.noreply.github.com>
2023-02-01 22:24:02 +0000

    Update inverseclient.py

--- inverseclient.py
+++ inverseclient.py
@@ -1,14 +1,14 @@
 import socket
 import threading
 import os
-IP = input("input ip: ")
+IP = input("input ip: ")    # server IP input
 print("default port 226")
-HOST = IP     #server IP address
-PORT = 226    #server port
+HOST = IP     # server IP address
+PORT = 226    # server port
 
 def receive_messages():
     while True:
-        #get messages from server
+        # get messages from server
         message = s.recv(4096).decode()
         if not message:
             break
@@ -16,15 +16,15 @@ def receive_messages():
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    #welcome message and prompt for a username
+    # welcome message and prompt for a username
     print(s.recv(1024).decode())
     username = input('username: ')
     s.sendall(username.encode())
-    #Start a new thread to receive messages
+    # start a new thread to receive messages
     receive_thread = threading.Thread(target=receive_messages)
     receive_thread.start()
     while True:
-        #Send messages to da server
+        # Send messages to da server
         message = input('{username}:')
         s.sendall(message.encode())
 
