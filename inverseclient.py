a9dd13ae1e8112ce4c1d31d4c07008aa2ccd0ef6
TheFakeFrog <116329407+theFakeFrog@users.noreply.github.com>
2023-02-08 02:45:16 +0000

    Update inverseclient.py

--- inverseclient.py
+++ inverseclient.py
@@ -1,60 +1,59 @@
 import socket
-import sys
-from plyer import notification
 import threading
 import json
-print("inverse 2.6")
-# Load config file
-with open("userconfig.json") as f:
+print("inverse v2.6.1")
+# Load configuration from file
+with open("config.json") as f:
     config = json.load(f)
 
-    federated_servers = config["federated_servers"]
+federated_servers = config["federated_servers"]
 
-print("Enter the federated server ID you want to connect to:")
-for i, (host, port) in enumerate(federated_servers):
-    print(f"{i}: {host}:{port}")
+# Stores clients and server info
+clients = {}
 
-server_id = int(input().strip())
-host, port = federated_servers[server_id]
+# Handling incoming connections
+def handle_client(conn, addr, server_id):
+    with conn:
+        print(f'Connected from server ID {server_id}')
+        conn.sendall(b'Connected to server!')  # welcome msg
+        username = conn.recv(1024).decode()
+        if len(username) > 20:
+            conn.sendall(b'Username too long. Maximum 20 characters allowed.')
+            conn.close()
+            return
+        # Add client to the dictionary with their username and server
+        clients[conn] = (username, server_id)
+        print(f'{username} has joined')
+        # Broadcast a message to clients to notify of the new user
+        for client, (client_username, client_server_id) in clients.items():
+            client.sendall(f'{username} has joined the chat from server {server_id}'.encode())
+        try:
+            while True:
+                data = conn.recv(4096)
+                if not data:
+                    raise Exception('Client disconnected')
+                # Broadcast the message to all clients
+                for client, (client_username, client_server_id) in clients.items():
+                    client.sendall(f'{username}${server_id}: {data.decode()}'.encode())
+        except Exception as e:
+            print(e)
+        # Remove the client and notify other clients
+        del clients[conn]
+        print(f'{username} has left the chat')
+        for client, (client_username, client_server_id) in clients.items():
+            client.sendall(f'{username} has left the chat from server {server_id}'.encode())
 
-notification.notify(
-    title="inverse client",
-    message=f"connected to server ID {i}",
-#    app_icon='inverse.png',
-    app_name="inverse",
-    timeout=5
-)
+def handle_federated_server(server_id, host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f'Listening on {host}:{port}...')
+        while True:
+            conn, addr = s.accept()
+            # Make a new thread for each connection
+            threading.Thread(target=handle_client, args=(conn, addr, server_id)).start()
 
-def receive_messages(s):
-    while True:
-        # get messages from server
-        message = s.recv(4096).decode()
-        if not message:
-            break
-        print(message)
-        notification.notify(
-    title="inverse client",
-    message=f"{message}",
-#    app_icon='inverse.png',
-    app_name="inverse",
-    timeout=5
-)
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
-    # welcome message and prompt for a username
-    print(s.recv(1024).decode())
-    username = input('username: ')
-    s.sendall(username.encode())
-    # start a new thread to receive messages
-    receive_thread = threading.Thread(target=receive_messages, args=(s,))
-    receive_thread.start()
-    while True:
-        # Send messages to da server
-        message = input(':')
-        s.sendall(message.encode())
-
-    s.close()
-    print("Connection closed")
+# Create and start the threads for each server
+for server_id, (host, port) in enumerate(federated_servers):
+    threading.Thread(target=handle_federated_server, args=(server_id, host, port)).start()
 
